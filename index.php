5fdfca6a9bcffe1a950e02ce406823508b9db0d2
Ouassim abbari <wassim.abbari@gmail.com>
2020-08-12 06:10:37 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,14 @@
 <?php
 echo "Hello world /n";
-echo $_REQUEST['data']['FIELDS']['ID'];
+
 $myfile = fopen("data.txt", "r") or die("Unable to open file!");
 echo fread($myfile,filesize("data.txt"));
 fclose($myfile);
 
-print_r($_REQUEST);
-writeToLog($_REQUEST, 'incoming');
-
-/**
- * Write data to log file.
- *
- * @param mixed $data
- * @param string $title
- *
- * @return bool
- */
-function writeToLog($data, $title = '') {
- $log = "\n------------------------\n";
- $log .= date("Y.m.d G:i:s") . "\n";
- $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
- $log .= print_r($data, 1);
- $log .= "\n------------------------\n";
- $log .= "id: ";
- $log .= $ _REQUEST ['data'] ['FIELDS'] ['ID'];
- $log .= "id : ";
- $log .= $ _REQUEST['FIELDS']['ID'];
- $log .= "event : ";
- $log .= $ _REQUEST['event'];
- file_put_contents(getcwd() . '/hook.log', $log, FILE_APPEND);
+if(!empty($_REQUEST)){
+ $log = $_REQUEST['event'];
  $fp = fopen('data.txt', 'a');
  fwrite($fp, $log);
  fclose($fp);
- return true;
-} 
-
+}
 ?>
