3ec82d08ef4968324cbd1b45654b40ae76336bec
Ouassim abbari <wassim.abbari@gmail.com>
2020-08-12 05:22:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-echo "Hello world";
+echo "Hello world \n";
+$myfile = fopen("data.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("data.txt"));
+fclose($myfile);
 
 print_r($_REQUEST);
 writeToLog($_REQUEST, 'incoming');
