929f37887011a49c1867081703c3deeca75f29b9
root <root@ubuntu.europe-west6-a.c.ubuntu-286707.internal>
2020-08-19 11:04:03 +0000

    new attempt

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 $myfile = fopen("data.txt", "r") or die("Unable to open file!");
 echo fread($myfile,filesize("data.txt"));
 fclose($myfile);
@@ -9,6 +9,7 @@ if(!empty($_REQUEST)){
  $fp = fopen('data.txt', 'a');
  fwrite($fp, $log);
  fclose($fp);
+ file_put_contents('data.txt', print_r($_REQUEST, true));
 }
 
 ?>
